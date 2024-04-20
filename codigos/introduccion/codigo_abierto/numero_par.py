08fa64077c97e4bb87e805bc56f7069116b3e61f
juanjimenez <juan.jimenez@fis.ucm.es>
2024-04-20 19:07:08 +0200

    numero_par.py bug removed

--- codigos/introduccion/codigo_abierto/numero_par.py
+++ codigos/introduccion/codigo_abierto/numero_par.py
@@ -3,7 +3,7 @@
 """
 Created on Fri Apr 19 15:31:19 2024
 Script para crear una función que comprueba si un número es par
-Script to create a function tha checks if a numver is pair
+Script to create a function tha checks if a number is even
 @author: juan
 """
 
@@ -11,10 +11,10 @@ def par(n):
     ''' 
     Esta función toma como entrada un número n. Si n es par devuelve un mensaje
     indicando que el número es par.
-    This funtion takes an integer number n as imput. If n is pair then
-    it shows a message indicating the number is pair.
+    This funtion takes an integer number n as imput. If n is even then
+    it shows a message indicating the number is even.
     '''
     if n%2 == 0:
         print(str(n),' es un numero par')
-        print(str(n),'is a pair number')
+        print(str(n),'is an even number')
     return()
