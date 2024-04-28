101f0d89124ed2fa51c04409bbf71081a70082ef
juanjimenez <juan.jimenez@fis.ucm.es>
2024-04-28 10:02:02 +0200

    correcciones en el código del ejemplo de if anidado

--- codigos/introduccion/codigo_abierto/numero_parytres_nested.py
+++ codigos/introduccion/codigo_abierto/numero_parytres_nested.py
@@ -3,9 +3,12 @@
 """
 Created on Fri Apr 19 15:31:19 2024
 Script para crear una función que comprueba si un número es par
+Si lo es, tiene un if anidado para probar si además es divisible entre tres
 si no lo es comprueba si es divisible por 3.
-Script to create a function tha checks if a number is even else,
-if can be divided by 3
+
+Script to create a function tha checks if a number is even and if it is so,
+checks if it is divisible by three to.  else, the program checks if the number
+can be divided by 3
 @author: juan
 """
 
@@ -13,16 +16,18 @@ def parthreenested(n):
     ''' 
     Esta función toma como entrada un número n. Si n es par devuelve un mensaje
     indicando que el número es par.
-    Si no, compr
+    Si no, comp
     This funtion takes an integer number n as imput. If n is even then
     it shows a message indicating the number is even.
     '''
     if n%2 == 0:
-        print(str(n),' es un numero par')
-        print(str(n),'is an even number')
+       
         if n%3 == 0:
-            print(str(n),' es un par numero divisible entre 3')
+            print(str(n),' es un numero par y divisible entre 3')
             print(str(n),'is an even & divisible by 3 number')
+        else:
+            print(str(n),' es un numero par')
+            print(str(n),'is an even number')
     elif n%3 == 0:
         print(str(n),' es un numero divisible entre 3')
         print(str(n),'is an number divisible by 3')
