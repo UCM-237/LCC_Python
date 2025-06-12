b043d6bec7635144cfc48ab8b4a708b76cc8880a
Juan Jiménez <juan.jimenez@fis.ucm.es>
2025-06-12 16:03:07 +0200

    casi acabado capítulo de datos

--- codigos/tratamiento_datos/datos_electrica_pandas.py
+++ codigos/tratamiento_datos/datos_electrica_pandas.py
@@ -15,27 +15,29 @@ generacion_mw.drop([0,1],inplace=True)
 lista = generacion_mw.columns
 for i in lista[1:]:
     generacion_mw[i] = generacion_mw[i].astype(float)
-    
+#hacemos un analisis estadístico general    
 des = generacion_mw.describe()
-des.loc['mean'][des.loc['mean']>0].plot.pie()
 
+#hacemos un diagrama de pastel con las medias de la producción
+ax=des.loc['mean'][des.loc['mean']>0].plot.pie(rotatelabels=True)
 #cambio los índices a las horas que es bastante sensato
 gn_Mw_h = generacion_mw.set_index('Hora')
 
 #pido que me grafique todas, frente al tiempo queda un tanto faragoso
 #pero da una idea de lo que hay
 pl.figure()
-gn_Mw_h.plot(rot=40,ylabel='Mw')
+ax = gn_Mw_h.plot(rot=40,ylabel='Mw')
+ax.legend(loc='upper left', bbox_to_anchor=(1.0, 1.0))
 pl.figure()
-gn_Mw_h.boxplot(rot=90)
-#vamos a hacer un diagrama de quesos pero eliminado los negativos que propiamente
+gn_Mw_h.boxplot(rot=90,ylabel='Mw')
+#vamos a hacer un diagrama de pastel pero eliminado los negativos que propiamente
 #no son produccion Seleccionamos una hora
 
-datos1005 = gn_Mw_h.loc['2025-03-19 01:05','Eólica':]
-datos1230 = gn_Mw_h.loc['2025-03-19 12:30','Eólica':]
+datos1005 = gn_Mw_h.loc['2025-03-19 01:05']
+datos1230 = gn_Mw_h.loc['2025-03-19 12:30']
 datosprod1005 = datos1005[datos1005>0]
 datosprod1230 = datos1230[datos1230>0]
 pl.figure()
-datosprod1005.plot.pie()
+datosprod1005.plot.pie(rotatelabels=True)
 pl.figure()
-datosprod1230.plot.pie()
+datosprod1230.plot.pie(rotatelabels=True)
